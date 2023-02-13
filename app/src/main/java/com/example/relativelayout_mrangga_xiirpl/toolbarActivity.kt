83920f97bb306c01356cfb690a9a48570b8229e5
MRanggaidradifadillah <ranggaif25@gmail.com>
2023-02-13 15:38:52 +0700

    Menambahkan Android NetWorking & Adapter

---
+++ app/src/main/java/com/example/relativelayout_mrangga_xiirpl/toolbarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.relativelayout_mrangga_xiirpl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class toolbarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toolbar)
+    }
+}
